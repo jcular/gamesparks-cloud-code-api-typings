1bd271cd35dbd34531a095780cb231dca579cf9d
johnsoncodehk <johnsoncodehk@gmail.com>
2018-03-18 18:01:11 +0800

    Add global declare file

---
+++ typings/Global.d.ts
@@ -0,0 +1,11 @@
+declare type date = any;
+declare type XMLObject = any;
+declare type SparkPendingMatch = any;
+declare type ScriptableObject = any;
+declare type List = any[];
+
+declare let Spark: Spark;
+
+interface Spark {
+    runtimeCollection(collectionName: string): SparkMongoCollectionReadWrite
+}
