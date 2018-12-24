0cebb950963c76f9613ec4375c3f104359255803
johnsoncodehk <johnsoncodehk@gmail.com>
2018-12-24 19:01:24 +0800

    Update runtimeCollection

--- typings/SparkCloudCode.d.ts
+++ typings/SparkCloudCode.d.ts
@@ -7,5 +7,12 @@ declare type List = any[];
 declare const Spark: Spark;
 
 interface Spark {
-    runtimeCollection(collectionName: string): SparkMongoCollectionReadWrite // Obsolete
+    /**
+     * Gets a runtimedata collection by name, this collection is read write access and can be queried using the methods defined in the SparkMongoCollectionReadWrite object.
+     * @validity All Scripts
+     * @param collectionName the name of the collection you wish to access
+     * @example var myRuntimeCollection = Spark.runtimeCollection("runtimetest");
+     * @deprecated use [Game Data Service](https://docs.gamesparks.com/documentation/configurator/game-data.html)
+     */
+    runtimeCollection(collectionName: string): SparkMongoCollectionReadWrite
 }
